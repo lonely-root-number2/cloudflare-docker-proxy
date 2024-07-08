6d7433b9763092e51cd947aeb46b2e056f65b907
lonely-root-number2 <x_1325792089@163.com>
2024-07-08 23:27:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.docker-proxy.4fan.top": dockerHub,
+  "quay.docker-proxy.4fan.top": "https://quay.io",
+  "gcr.docker-proxy.4fan.top": "https://gcr.io",
+  "k8s-gcr.docker-proxy.4fan.top": "https://k8s.gcr.io",
+  "k8s.docker-proxy.4fan.top": "https://registry.k8s.io",
+  "ghcr.docker-proxy.4fan.top": "https://ghcr.io",
+  "cloudsmith.docker-proxy.4fan.top": "https://docker.cloudsmith.io",
+  "ecr.docker-proxy.4fan.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.docker-proxy.4fan.top": dockerHub,
 };
 
 function routeByHosts(host) {
